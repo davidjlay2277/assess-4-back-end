4c7da59914ef1ba6177bc9a6a00d69cba742955c
David Lay <davidjlay2277@gmail.com>
2023-07-10 20:36:23 -0500

    front end progress

--- client/main.js
+++ client/main.js
@@ -1,28 +1,60 @@
 // const { getFortune } = require("../server/controller");
-console.log('front end running')
+console.log("front end running");
 
-const complimentBtn = document.getElementById("complimentButton")
-const fortuneBtn = document.getElementById("fortuneButton")
-// create a base URL varible for the site
-baseURL =   'http://localhost:4000/api'
+//buttons
+const complimentBtn = document.getElementById("complimentButton");
+const complimentType = document.getElementById("complimentInput");
 
-const getCompliment = () => {
- 
-    axios.get(`${baseURL}/compliment`)
-        .then(res => {
-            const data = res.data;
-            alert(data);
-    });
+const fortuneBtn = document.getElementById("fortuneButton");
+
+const messageBtn = document.getElementById("messageButton");
+const messageInput = document.getElementById("")
+
+const imgBtn = document.getElementById("imgButton");
+const rouletBtn = document.getElementById("rouletButton");
+
+console.log(rouletBtn);
+baseURL = "http://localhost:4000/api";
+
+//function for compliment that runs when clicked
+// take in the form value, send to server on end point /compliment
+const getCompliment = (e) => {
+  e.preventDefault();
+  const complimentType = document.getElementById('complimentInput')
+  axios.post(`${baseURL}/compliment`).then((res) => {
+    const data = res.data;
+   
+    console.log(complimentType.value)
+    alert("hit on complaint");
+  });
+};
+
+const getMessage = (e) => {
+  e.preventDefault();
+  console.log("hit on message button");
+  alert("hit on message button");
+};
+
+const getImg = (e) => {
+  e.preventDefault();
+  console.log("hit on image button");
+  axios.post(`${baseURL}/img`).then((res) => {});
+  alert("hit on image button");
 };
 
 const getFortune = () => {
-
-    axios.get(`${baseURL}/fortune`)
-        .then(res => {
-            const data = res.data;
-            alert(data);
-    });
+  axios.get(`${baseURL}/fortune`).then((res) => {
+    alert("hit on fortune button");
+  });
 };
 
-complimentBtn.addEventListener('click', getCompliment)
-fortuneBtn.addEventListener('click', getFortune)
+const roulet = () => {
+
+  alert("hit on roulet btn");
+};
+
+complimentBtn.addEventListener("click", getCompliment);
+fortuneBtn.addEventListener("click", getFortune);
+messageBtn.addEventListener("click", getMessage);
+imgBtn.addEventListener("click", getImg);
+rouletBtn.addEventListener("click", roulet);
--- server/controller.js
+++ server/controller.js
@@ -3,28 +3,29 @@
 module.exports = {
   getCompliment: (req, res) => {
     const compliments = [
-      "Gee, you're a smart cookie!",
-      "Cool shirt!",
-      "Your Javascript skills are stellar.",
+      "Welcome to the internet, we're happy you're here!",
+      "Code so clean, I could eat browser cookies off of it",
+      "The symmetry of your face is uncanny",
+      "You click buttons like a pro!",
     ];
 
     // choose random compliment
     let randomIndex = Math.floor(Math.random() * compliments.length);
     let randomCompliment = compliments[randomIndex];
-
     res.status(200).send(randomCompliment);
   },
   getFortune: (req, res) => {
     const fortunes = [
-      "A soft voice may be awfully persuasive.",
-      "Adventure can be real happiness.",
-      "A truly rich life contains love and art in abundance.",
+      "A System crash is immenenat, better push to GitHub",
+      "Your next assessment will score 99.",
+      "Callback Hell is a real place, but you can avoid it in this space",
+      "This greeting card will make no sense",
+      "Agents! They cut the hardline! ... either that, or cors is not installed",
     ];
 
     // choose random fortune
     let randomIndex = Math.floor(Math.random() * fortunes.length);
     let randomFortune = fortunes[randomIndex];
-
     res.status(200).send(randomFortune);
   },
 };
